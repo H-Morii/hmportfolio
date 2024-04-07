c9073b18ea52899f26a9662fc95ed2e79fa47ae4
H-morii <haruki.mori21@gmail.com>
2024-04-07 12:46:01 +0900

    Title completed.

--- src/components/components/Title/Title.tsx
+++ src/components/components/Title/Title.tsx
@@ -1,8 +1,15 @@
-
 const Title = () => {
   return (
     <>
-      
+      <section className="flex justify-center flex-col md:items-end ">
+        <h2 className=" text-[3rem] md:text-[4rem] p-2">SOFTWARE <br/><span className="pl-6">ENGINEER</span></h2>
+        <div className="text-[0.8rem]">
+        <h3 className=" text-left p-4 md:p-1">I am a skilled engineer developing software solution. <br />
+            I love creating a app that can simplify work to be more efficient. <br />
+            Committed to lifelong learning and professional growth, <br /> I bring both technical skills and a collaborative spirit to every project I work on.
+        </h3>
+        </div>
+      </section>
     </>
   )
 }
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,8 +1,12 @@
 import Navbar from "../components/components/Navbar/Navbar"
+import Title from "../components/components/Title/Title"
 
 const Home = () => {
   return (
+    <>
     <Navbar/>
+    <Title/>
+    </>
   )
 }
 
